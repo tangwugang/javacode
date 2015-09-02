6013fdfa72671db54df936507b2966b3026f1947
twg <wugang.tang@tqmall.com>
2015-09-02 09:46:47 +0800

    添加监听器

---
+++ mini-core/src/main/java/cc/ichoice/listener/MyRequestContextListener.java
@@ -0,0 +1,12 @@
+package cc.ichoice.listener;
+
+import org.springframework.web.context.request.RequestContextListener;
+
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Created by twg on 15/9/2.
+ */
+@WebListener
+public class MyRequestContextListener extends RequestContextListener {
+}
